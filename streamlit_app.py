cd1fef61290f86d456ce840bb58928b437c273fd
Vyshnavi243 <vyshnavi.gaddam@feuji.com>
2024-07-12 02:15:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ ingredients_list = st.multiselect(
 
 if ingredients_list :
     ingredients_string = ''
-    for fruit_choosen in ingredients_list:
-        ingredients_string += fruit_choosen + ' '
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width =True)
