be6a0135864bd5f01b08db28d92867365c4e42b1
Vyshnavi243 <vyshnavi.gaddam@feuji.com>
2024-07-11 19:35:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
     INSERT INTO smoothies.public.orders (ingredients, name_on_order)
     VALUES ('{ingredients_string}', '{name_on_order}')
     """
-    st.subheader(fruit_chosen + 'Nutrition Information')
+    st.subheader(name_on_order + 'Nutrition Information')
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +name_on_order")
     fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     st.write(my_insert_stmt)
